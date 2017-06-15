115376be3b5d4fae3345a681a75f3632d1d6f35c
Tyler Baker <tyler.baker@linaro.org>
2017-06-15 15:39:02 -0700

    Add helper to check rollout status
    Signed-off-by: Tyler Baker <tyler.baker@linaro.org>

---
+++ check_rollout_status.py
@@ -0,0 +1,51 @@
+import argparse
+import requests
+import json
+import time
+
+__version__ = 1.0
+
+user = 'admin'
+password  = 'admin'
+
+
+def rollout(id, hostname, xfail=False):
+    headers = { 'Content-Type': 'application/hal+json;charset=UTF-8',
+                'Accept': 'application/hal+json' }
+    rollout_url = 'http://%s:8080/rest/v1/rollouts/%s' % (hostname, id)
+    response = requests.get(rollout_url, auth=(user, password), headers=headers)
+    if response.status_code != 500:
+        data = json.loads(response.content)
+        print data
+        for key in data:
+            if 'totalTargets' == key:
+                total_targets = data[key]
+            if 'totalTargetsPerStatus' == key:
+                target_error_count = data[key]['error']
+
+        if total_targets == target_error_count:
+            if xfail:
+                print "Rollout failed with errors, but this was expected."
+                exit(0)
+            else:
+                print "Rollout failed with errors."
+                exit(1)
+        else:
+            if xfail:
+                print "Rollout succeeded with no errors, this was unexpected."
+                exit(1)
+            else:
+                print "Rollout succeeded with no errors."
+                exit(0)
+
+def main():
+    description = 'Simple Hawkbit API Wrapper for checking rollout status'
+    parser = argparse.ArgumentParser(version=__version__, description=description)
+    parser.add_argument('-i', '--id', help='Rollout ID', type=int, required=True)
+    parser.add_argument('-x', '--xfail', help='Inverts the return code if a failure is expected', action="store_true")
+    parser.add_argument('-host', '--hostname', help='Hawkbit Server Hostname or IP', default='hawkbit')
+    args = parser.parse_args()
+    rollout(args.id, args.hostname, args.xfail)
+
+if __name__ == '__main__':
+    main()
